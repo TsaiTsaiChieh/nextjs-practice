be7e3663395f0299f03af7d9da5d1ce6800eeac4
Tsai-Chieh <d0177381@gmail.com>
2023-07-12 16:02:15 +0800

    :tada: image explained

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Image src={Hero} alt="Hero" className={styles.img}/>
+    </div>
   )
 }
