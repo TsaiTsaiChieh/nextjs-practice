a038afc39c67908dcf6ab39d3ea4068fc20cb233
Tsai-Chieh <d0177381@gmail.com>
2023-07-12 11:34:49 +0800

    :tada: server side & client side

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 
@@ -18,6 +19,9 @@ const Navbar = () => {
       {links.map((link) => (
         <Link key={link.id} href={link.url}>{link.title}</Link>
       ))}
+      <button onClick={()=> {
+        console.log('Logout')
+      }}>Logout</button>
       </div>
     </div>
   )
