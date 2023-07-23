97c3d14cdb201d49f6d743359ed38ec82ea86038
Tsai-Chieh <d0177381@gmail.com>
2023-07-23 15:12:48 +0800

    :sparkles: fetch data in client side (SWR)

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+"use client"
 import styles from './page.module.css'
+import useSWR from 'swr'
 
 const Dashboard = () => {
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
   return (
     <div className={styles.container}>Dashboard</div>
   )
