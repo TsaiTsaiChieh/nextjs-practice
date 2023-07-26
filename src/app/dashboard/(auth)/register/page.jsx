833e02fdcf119d13c70c5652a2a42b524c8ee4be
Tsai-Chieh <d0177381@gmail.com>
2023-07-26 16:59:40 +0800

    :construction: work in progress

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+"use client"
+import Link from 'next/link'
+import styles from './page.module.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input className={styles.input} type="text" placeholder='username' required />
+        <input className={styles.input} type="email" placeholder='email' required />
+        <input className={styles.input} type="password" placeholder='password' required />
+        <button className={styles.button}>Register</button>
+      </form>
+      <Link href="/dashboard/login">Login with an existing account</Link>
+    </div>
   )
 }
 
